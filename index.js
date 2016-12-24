a16c7214a9faca1172a0e02ea7ae9c9bdfe29eab
YaronSegal <seg.yar@gmail.com>
2016-12-24 11:27:53 +0200

    added production configuration

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var config  = require('config');
-var cpuMonitor = require('./lib/monitors/cpuMonitor.js');
-var memMonitor = require('./lib/monitors/memMonitor.js');
+var util    = require('util');
+var pjson   = require('./package.json');
+var cpuMonitor  = require('./lib/monitors/cpuMonitor.js');
+var memMonitor  = require('./lib/monitors/memMonitor.js');
 var diskMonitor = require('./lib/monitors/diskMonitor.js');
 
 var timeIntervalMsec = config.get('timeIntervalMsec');
+
+cl(util.format("**************** starting %s v%s ****************", pjson.name, pjson.version));
+cl(util.format("NODE_ENV = %s", process.env.NODE_ENV));
+cl("\n");
 
 cpuMonitor.monitor(timeIntervalMsec.cpu);
 memMonitor.monitor(timeIntervalMsec.memory);
--- update.sh
+++ update.sh
@@ -8,10 +8,13 @@ if [ $# -eq 0 ]
 	exit
 fi
 
-pm2 stop $1
-git checkout -f config.json
+#pm2 stop $1
+#git checkout -f config.json
 git pull origin
 npm install
-pm2 start $1
+npm run startProd
+#export NODE_ENV=production
+#pm2 start $1
+pm2 reload $1
 
 echo "Done!"
